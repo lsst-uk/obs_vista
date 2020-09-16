f1e28fa6031c78650c779d929d1dcce6db764c20
Raphael Shirley <Raphael.Shirley@sussex.ac.uk>
2020-09-16 12:28:51 +0100

    Current settings for PSF determination and photoCal

--- config/calibrate.py
+++ config/calibrate.py
@@ -14,7 +14,7 @@ ObsConfigDir = os.path.dirname(__file__)
 
 # Reference catalogs
 #The following was copied from obs_subaru and manages conflicts between gen2 and gen3
-ref_cat = "ps1_pv3_3pi_20170110_vista" #_vista
+ref_cat = "ps1_pv3_3pi_20170110_2mass" #_vista
 for refObjLoader in (config.astromRefObjLoader,
                      config.photoRefObjLoader,
                      ):
@@ -25,32 +25,55 @@ for refObjLoader in (config.astromRefObjLoader,
 # These are the Gen3 configuration options for reference catalog name.
 config.connections.photoRefCat = ref_cat
 config.connections.astromRefCat = ref_cat
+# These are gen2?:
+config.photoCal.applyColorTerms=True
+config.photoCal.photoCatName=ref_cat
 
-# These colorterms are for HSC, included as an example
+# Taken from https://github.com/lsst/pipe_tasks/blob/master/python/lsst/pipe/tasks/colorterms.py:
+# p' = primary + c0 + c1*(primary - secondary) + c2*(primary - secondary)**2
+# VISTA-2MASS colour terms taken from https://arxiv.org/abs/1711.08805 eqn 5-9
+# Z_V = J_2 + (0.86 ± 0.08) · (J − Ks)_2
+# Y_V = J_2 + (0.46 ± 0.02) · (J − Ks)_2
+# J_V = J_2 − (0.031 ± 0.006) · (J − Ks)_2
+# H_V = H_2 + (0.015 ± 0.005) · (J − Ks)_2
+# Ks_V = Ks_2 − (0.006 ± 0.007) · (J − Ks)_2
+
 colorterms = config.photoCal.colorterms
 from lsst.pipe.tasks.colorterms import ColortermDict, Colorterm
 colorterms.data["ps1*"] = ColortermDict(data={
-    'g': Colorterm(primary="g", secondary="r", 
+    #####HSC COLOUR TERMS FROM obs_subaru
+    'HSC-G': Colorterm(primary="g", secondary="r", 
     c0=0.00730066, c1=0.06508481, c2=-0.01510570),
-    'r': Colorterm(primary="r", secondary="i", 
+    'HSC-R': Colorterm(primary="r", secondary="i", 
     c0=0.00279757, c1=0.02093734, c2=-0.01877566),
-    'i': Colorterm(primary="i", secondary="z", 
+    'HSC-I': Colorterm(primary="i", secondary="z", 
     c0=0.00166891, c1=-0.13944659, c2=-0.03034094),
-    #'z2': Colorterm(primary="z", secondary="y", 
-    #c0=-0.00907517, c1=-0.28840221, c2=-0.00316369),
-    #'y2': Colorterm(primary="y", secondary="z", 
-    #c0=-0.00156858, c1=0.14747401, c2=0.02880125),
-    'z': Colorterm(primary="z", secondary="y", 
-    c0=-0.0, c1=-0.0, c2=-0.0),
-    'y': Colorterm(primary="y", secondary="z", 
-    c0=-0.0, c1=0.0, c2=0.0),
-    'j': Colorterm(primary="j", secondary="y",   #PLACEHOLDER
-    c0=-0.0, c1=0.0, c2=0.0),
-    'h': Colorterm(primary="h", secondary="y",   #PLACEHOLDER
-    c0=-0.0, c1=0.0, c2=0.0),
-    'ks': Colorterm(primary="ks", secondary="y", #PLACEHOLDER
-    c0=-0.0, c1=0.0, c2=0.0),
-   
+    'HSC-Z': Colorterm(primary="z", secondary="y", 
+    c0=-0.00907517, c1=-0.28840221, c2=-0.00316369),
+    'HSC-Y': Colorterm(primary="y", secondary="z", 
+    c0=-0.00156858, c1=0.14747401, c2=0.02880125),
+    ####VISTA REF (NO COLOUR TERMS)
+    #'z': Colorterm(primary="z", secondary="y", 
+    #c0=-0.0, c1=-0.0, c2=-0.0),
+    #'y': Colorterm(primary="y", secondary="z", 
+    #c0=-0.0, c1=0.0, c2=0.0),
+    #'j': Colorterm(primary="j", secondary="y",  
+    #c0=-0.0, c1=0.0, c2=0.0),
+    #'h': Colorterm(primary="h", secondary="y",   
+    #c0=-0.0, c1=0.0, c2=0.0),
+    #'ks': Colorterm(primary="ks", secondary="y", 
+    #c0=-0.0, c1=0.0, c2=0.0),
+    ####2MASS COLOUR TERMS - all from J, Ks - see above
+    'VISTA-Z': Colorterm(primary="j", secondary="ks", 
+    c0=0.0, c1=0.86, c2=-0.0),
+    'VISTA-Y': Colorterm(primary="j", secondary="ks", 
+    c0=0.0, c1=0.46, c2=0.0),
+    'VISTA-J': Colorterm(primary="j", secondary="ks",   
+    c0=0.0, c1=0.031, c2=0.0),
+    'VISTA-H': Colorterm(primary="j", secondary="ks",   
+    c0=0.0, c1=0.015, c2=0.0),
+    'VISTA-Ks': Colorterm(primary="j", secondary="ks", 
+    c0=0.0, c1=-0.006, c2=0.0),
 })
 # For the HSC r2 and i2 filters, use the r and i values from the catalog
 # for refObjLoader in (config.calibrate.astromRefObjLoader,
