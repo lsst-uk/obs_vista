aea4d1d68394366261bb441f342d0742c911e509
Raphael Shirley <raphael.shirley@hotmail.co.uk>
2021-04-22 17:18:48 +0100

    add multiband driver config to allow restarts

---
+++ config/multiBandDriver.py
@@ -0,0 +1,14 @@
+import os.path
+from lsst.utils import getPackageDir
+from lsst.meas.algorithms import SourceDetectionTask
+
+for sub in (
+    "deblendCoaddSources",
+    "detectCoaddSources",
+    "mergeCoaddDetections",
+    "measureCoaddSources",
+    "mergeCoaddMeasurements",
+    "forcedPhotCoadd"):
+    path = os.path.join(getPackageDir("obs_vista"), "config", sub + ".py")
+    if os.path.exists(path):
+        getattr(config, sub).load(path)
