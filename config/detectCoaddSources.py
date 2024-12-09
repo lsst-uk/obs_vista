b40c1a2b51874c9de075c8407ce6513f4642d168
Elham Saremi <saremi_elham@yahoo.com>
2024-12-09 13:03:37 +0000

    Reduce background mesh size for detection on coadds
    Until now, only the threshold type and value for detections were
    defined in this config.
    
    With this commit, I've reduced the background mesh size for detection
    on coadds by setting binSize=128 pixels, consistent with
    obs_subaru. This adjustment reduces the load on the deblender and
    improves detection and photometry quality for small, nearly unresolved
    sources. For those focusing on large sources, the background can be
    restored for specialized measurements.

--- config/detectCoaddSources.py
+++ config/detectCoaddSources.py
@@ -1,7 +1,14 @@
+config.detection.doTempWideBackground = True
+config.detection.tempWideBackground.binSize = 128
+config.detection.tempWideBackground.useApprox = False
+config.detection.reEstimateBackground = True
+config.detection.background.binSize = 128
+config.detection.background.useApprox = False
+
+
 # Not enough detections?
 # https://community.lsst.org/t/detectcoaddsource-detection-threshold-on-noisy-background-data/3050
 #from lsst.meas.algorithms import SourceDetectionTask
 #config.detection.retarget(SourceDetectionTask) #use simpler detection algorithm
 config.detection.thresholdType='stdev' #default='pixel_stdev'
 config.detection.thresholdValue=10.0 #default=5. 5 leads to large detected areas possibly due to variance scaling
-
