034293e9241de56ad22608b9517e99fd5d31c9fc
Elham Saremi <saremi_elham@yahoo.com>
2024-12-16 16:25:09 +0000

    Switch to CalibrateImageTask
    Until now, we used the "characterizeImage", "calibrate", and
    "writeSourceTable" tasks in step 1 of pipeline, but now the
    "calibrateImage" task has replaced all of them.
    
    "CalibrateImageTask" performs “single frame processing” on one single
    visit post-Instrument Signature Removal single detector exposure
    (postISRCCD). This involves repairing cosmic rays and defects,
    detecting and measuring sources on the exposure to make an initial
    estimation of the point spread function (PSF), re-doing detection and
    measurement with that fitted PSF to compute the astrometric and
    photometric calibrations, and computing summary statistics of the
    exposure and measured catalog.
    
    With this commit, I updated the DRP YAML file and added the
    "calibrateImage" configuration files to the configs.  This task also
    requires the "computeExposureSummaryStats.py" config to set parameters
    like "fiducialPsfSigma", "fiducialSkyBackground", and
    "fiducialZeroPoint".
    
    For now, I've commented these parameters out, and we can add them
    later once they are determined.

---
+++ config/calibrateImage.py
@@ -0,0 +1,30 @@
+import math
+import os.path
+
+from lsst.meas.algorithms import ColorLimit
+
+config_dir = os.path.dirname(__file__)
+
+# Maintain characterizeImage defaults while we work to understand differences
+# between this new Task and older runs.
+config.install_simple_psf.fwhm = 1.5*2.0*math.sqrt(2.0*math.log(2.0))
+config.psf_detection.thresholdType = "stdev"
+
+# Use PS1 for both astrometry and photometry.
+config.connections.astrometry_ref_cat = "ps1_pv3_3pi_20170110_vista"
+config.connections.photometry_ref_cat = "ps1_pv3_3pi_20170110_vista"
+config.astrometry_ref_loader.load(os.path.join(config_dir, "filterMap.py"))
+# Use the filterMap instead of the "any" filter (as is used for Gaia).
+config.astrometry_ref_loader.anyFilterMapsToThis = None
+config.photometry_ref_loader.load(os.path.join(config_dir, "filterMap.py"))
+
+# Use colorterms for photometric calibration, with color limits on the refcat.
+config.photometry.applyColorTerms = True
+config.photometry.photoCatName = "ps1_pv3_3pi_20170110_vista"
+#colors = config.photometry.match.referenceSelection.colorLimits
+#colors["g-r"] = ColorLimit(primary="g_flux", secondary="r_flux", minimum=0.0)
+#colors["r-i"] = ColorLimit(primary="r_flux", secondary="i_flux", maximum=0.5)
+config.photometry.colorterms.load(os.path.join(config_dir, "colorterms.py"))
+
+# Exposure summary stats
+config.compute_summary_stats.load(os.path.join(config_dir, "computeExposureSummaryStats.py"))
---
+++ config/computeExposureSummaryStats.py
@@ -0,0 +1,7 @@
+import os.path
+
+config_dir = os.path.dirname(__file__)
+
+#config.load(os.path.join(config_dir, "fiducialPsfSigma.py"))
+#config.load(os.path.join(config_dir, "fiducialSkyBackground.py"))
+#config.load(os.path.join(config_dir, "fiducialZeroPoint.py"))
