700d7323de55bebee41a4bbec0462a01d2698146
Elham Saremi <saremi_elham@yahoo.com>
2025-01-20 20:55:39 +0000

    Add skymaps for ComCam data
    Until now, we used only "hscPdr2" to create the skymap for HSC.
    
    With this commit, I generated skymap for ComCam data that named
    "lsst_cells_v1" using the following command:
    
    skymap $repo -C "$OBS_VISTA_DIR/config/makeLSSTSkyMap.py"  \
                 -c name="lsst_cells_v1"

--- config/makeLSSTSkyMap.py
+++ config/makeLSSTSkyMap.py
@@ -1,8 +1,7 @@
-# Taken from https://github.com/lsst/obs_lsst/blob/main/config/makeSkyMap.py
-# This may change
-config.name = "LSST"
+config.name = "lsst_cells_v1"
 config.skyMap.name = "rings"
 config.skyMap["rings"].numRings = 120
 config.skyMap["rings"].projection = "TAN"
-config.skyMap["rings"].tractOverlap = 1.0/60 # Overlap between tracts (degrees)
+config.skyMap["rings"].tractOverlap = 1.0/60
 config.skyMap["rings"].pixelScale = 0.2
+config.skyMap["rings"].tractBuilder.name = "cells"
