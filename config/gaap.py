c24f35e8a4c5d7d77356de9ad4d008fd4ee767f6
Elham Saremi <saremi_elham@yahoo.com>
2025-03-10 17:50:59 +0000

    Cleanup and update descriptions

--- config/gaap.py
+++ config/gaap.py
@@ -1,11 +1,15 @@
 # Enable GAaP (Gaussian Aperture and PSF) colors
 # 'config' is typically a SourceMeasurementConfig
-try:
-    import lsst.meas.extensions.gaap  # noqa
+#try:
+#    import lsst.meas.extensions.gaap  # noqa
 
-    config.plugins.names.add("ext_gaap_GaapFlux")
-    config.plugins["ext_gaap_GaapFlux"].sigmas = [0.5, 0.7, 1.0, 1.5, 2.5, 3.0]
+    # Ensure the GAaP plugin is registered with the measurement framework
+#    config.measurement.plugins.names.add("ext_gaap_GaapFlux")
+
+
+#    config.plugins.names.add("ext_gaap_GaapFlux")
+#    config.plugins["ext_gaap_GaapFlux"].sigmas = [0.5, 0.7, 1.0, 1.5, 2.5, 3.0]
     # Enable PSF photometry after PSF-Gaussianization for `ext_gaap_GaapFlux`
-    config.plugins["ext_gaap_GaapFlux"].doPsfPhotometry = True
-except ImportError as exc:
-    print("Cannot import lsst.meas.extensions.gaap (%s): disabling GAaP flux measurements" % (exc,))
+#    config.plugins["ext_gaap_GaapFlux"].doPsfPhotometry = True
+#except ImportError as exc:
+#    print("Cannot import lsst.meas.extensions.gaap (%s): disabling GAaP flux measurements" % (exc,))
