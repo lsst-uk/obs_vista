3664191b5b94ebc4d62f769d2c86722372612335
Elham Saremi <85132726+Saremi-Elham@users.noreply.github.com>
2025-05-25 11:23:14 +0100

    Update forcedPhotCoadd.py

--- config/forcedPhotCoadd.py
+++ config/forcedPhotCoadd.py
@@ -7,5 +7,5 @@ config.measurement.load(os.path.join(os.path.dirname(__file__), "kron.py"))
 config.measurement.load(os.path.join(os.path.dirname(__file__), "convolvedFluxes.py"))
 config.load(os.path.join(os.path.dirname(__file__), "cmodel.py"))
 
-config.measurement.plugins["base_PixelFlags"].masksFpAnywhere.remove("STREAK")
-config.measurement.plugins["base_PixelFlags"].masksFpCenter.remove("STREAK")
+#config.measurement.plugins["base_PixelFlags"].masksFpAnywhere.remove("STREAK")
+#config.measurement.plugins["base_PixelFlags"].masksFpCenter.remove("STREAK")
