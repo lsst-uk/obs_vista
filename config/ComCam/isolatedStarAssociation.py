e91bb583a5e55d30d7a28899d2ee836a35ffd27b
Elham Saremi <saremi_elham@yahoo.com>
2026-04-03 14:08:35 +0100

    Minor update bands for DP1 processing

--- config/ComCam/isolatedStarAssociation.py
+++ config/ComCam/isolatedStarAssociation.py
@@ -1,3 +1,3 @@
 # Use same band order for all instances of isolatedStarAssociation
 # and once here for all pipelines.
-config.band_order = ["i", "r", "g", "z", "y", "Z", "Y", "J", "H", "K"]
+config.band_order = ["i", "r", "z", "y", "g", "u", "Z", "Y", "J", "H", "K"]
