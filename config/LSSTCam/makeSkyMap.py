d12891ab81e8d5ed76fff0a6e57f13709d2537a9
Elham Saremi <85132726+Saremi-Elham@users.noreply.github.com>
2026-09-15 12:40:19 +0100

    Add DP2 lsst_cells_v2 skymap

---
+++ config/LSSTCam/makeSkyMap.py
@@ -0,0 +1,7 @@
+config.name = "lsst_cells_v2"
+config.skyMap.name = "rings"
+config.skyMap["rings"].numRings = 120
+config.skyMap["rings"].projection = "TAN"
+config.skyMap["rings"].tractOverlap = 1.0/60
+config.skyMap["rings"].pixelScale = 0.2
+config.skyMap["rings"].tractBuilder.name = "cells"
